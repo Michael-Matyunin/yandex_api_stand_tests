55be87157bc911e46e9a33c7884bc65422844459
Michael-Matyunin <matuninm2908@gmail.com>
2024-10-11 12:18:18 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,12 @@
+
+URL_SERVICE = "https://bb6b8fa4-04d5-4e71-8206-65ded3f00756.serverhub.praktikum-services.ru"
+
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+CREATE_USER_PATH = "/api/v1/users/"
+
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ sender_stand_request.py
@@ -0,0 +1,23 @@
+
+import configuration
+
+
+import requests
+
+import data
+
+def post_new_user(body):
+    # Выполнение POST-запроса с использованием URL из конфигурационного файла, тела запроса и заголовков
+    # URL_SERVICE и CREATE_USER_PATH объединяются для формирования полного URL для запроса
+    # json=body используется для отправки данных пользователя в формате JSON
+    # headers=data.headers устанавливает заголовки запроса из модуля data
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+
+def get_users_table():
+    # Составление полного URL пути к данным таблицы пользователей
+    # путем конкатенации базового URL сервиса и конечной точки таблицы пользователей
+    # Возвращает объект ответа от сервера
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
